38814408bbe2e41804ba25d3bce8d1b099f0a2d7
Dima <dima@Dietrichs-MacBook-Air.fritz.box>
2022-10-11 15:15:12 +0200

    fix links

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  aboutMe = [
+    {
+      icon: '../../assets/img/walk-line.png',
+      headline: 'headline',
+      text: 'text'
+    },
+    {
+      icon: '../../assets/img/heart-line.png',
+      headline: 'headline',
+      text: 'text'
+    },
+    {
+      icon: '../../assets/img/search-line.png',
+      headline: 'headline',
+      text: 'text'
+    },
+    {
+      icon: '../../assets/img/flight-takeoff-line.png',
+      headline: 'headline',
+      text: 'text'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skills = [
+    {
+      icon: '../../assets/img/java script.png',
+      name: 'Java script'
+    },
+    {
+      icon: '../../assets/img/angular-logo.png',
+      name: 'Angular'
+    },
+    {
+      icon: '../../assets/img/Group 205.png',
+      name: 'HTML/CSS'
+    },
+    {
+      icon: '../../assets/img/Group 226.png',
+      name: 'SCRUM'
+    },
+    {
+      icon: '../../assets/img/Git-Icon-1788C 2.png',
+      name: 'Git'
+    },
+    {
+      icon: '../../assets/img/Group 20.png',
+      name: 'Design Thinking'
+    },
+    {
+      icon: '../../assets/img/icons8-rest-api-80 2.png',
+      name: 'Rest API'
+    },
+    {
+      icon: '../../assets/img/icons8-test-lab-96 2.png',
+      name: 'Test automation'
+    },
+    {
+      icon: '../../assets/img/icons8-database-52 2.png',
+      name: 'Databases'
+    },
+  ];
+
 
   constructor() { }
 
