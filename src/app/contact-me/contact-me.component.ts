6f9ecd150882910daa94c4acbb11ec45662bf028
Dima <dima@Dietrichs-MacBook-Air.fritz.box>
2022-10-12 17:32:47 +0200

    fix responsive

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-me.component.scss']
 })
 export class ContactMeComponent implements OnInit {
+  @ViewChild('myForm') myForm: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendMail() {
+    //action="https://dietrich-richter.developerakademie.net/send_mail/send_mail.php"
+
   }
 
 }
