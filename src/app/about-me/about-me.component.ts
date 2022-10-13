41a23be45b016ec4ebf7faf1a01ccad359869dc7
Dima <dima@Dietrichs-Air.fritz.box>
2022-10-13 16:59:27 +0200

    fix about me im path

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -9,22 +9,22 @@ export class AboutMeComponent implements OnInit {
 
   aboutMe = [
     {
-      icon: '../../assets/img/walk-line.png',
+      icon: 'assets/img/walk-line.png',
       headline: 'I Started',
       text: 'my IT journey by trying to program websites in my free time and questioning how programs were created. After a few turtorials I discovered the fun of programming and wanted to deepen my knowledge and gain experience in web development.'
     },
     {
-      icon: '../../assets/img/heart-line.png',
+      icon: 'assets/img/heart-line.png',
       headline: 'My Search',
       text: 'After a while, I needed a common thread to learn faster and more efficiently, so I decided to seek professional help. I then came across the Developer Academy by chance and in December 2021 I decided to go down the path with the team. In addition to my main job, I learned the basic knowledge for frontend web development there in a very short time. I have completed many different projects there, both individually and in groups.'
     },
     {
-      icon: '../../assets/img/search-line.png',
+      icon: 'assets/img/search-line.png',
       headline: 'My Passion',
       text: "After the first modules I realized that I had made the right decision. When I encountered the first hurdles, I couldn't stop looking for a solution until I found it. With every project I complete and every challenge I overcome, I learn something new and I don't want to stop. I invest a lot of time in programming and I enjoy it"
     },
     {
-      icon: '../../assets/img/flight-takeoff-line.png',
+      icon: 'assets/img/flight-takeoff-line.png',
       headline: 'My Journey',
       text: 'I would like to deepen my knowledge and curiosity in the field of frontend development and am therefore looking for a chance to work in an interesting team.'
     },
